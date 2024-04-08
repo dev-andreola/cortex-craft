cef49610285c4661b1f211446f47774be0c941e1
vtAndreola <dev.andreola@gmail.com>
2024-04-08 18:52:39 -0300

    feat: Finish landing page

--- app/(marketing)/footer.tsx
+++ app/(marketing)/footer.tsx
@@ -1,7 +1,46 @@
+import { Button } from "@/components/ui/button";
+
+import { FaBrain, FaUserFriends } from "react-icons/fa";
+import { TbTicTac } from "react-icons/tb";
+import { GiProgression } from "react-icons/gi";
+
 export const Footer = () => {
   return (
     <footer className="hidden lg:block h-20 w-full border-t-2 border-slate-200 p-2">
-      Footer
+      <div className="max-w-screen-lg mx-auto flex items-center justify-evenly h-full">
+        <Button
+          size={"lg"}
+          variant={"ghost"}
+          className="w-full flex items-center justify-center gap-4"
+        >
+          <FaBrain size={24} />
+          Cérebro em Ação
+        </Button>
+        <Button
+          size={"lg"}
+          variant={"ghost"}
+          className="w-full flex items-center justify-center gap-4"
+        >
+          <TbTicTac size={24} />
+          Desafios Diários
+        </Button>
+        <Button
+          size={"lg"}
+          variant={"ghost"}
+          className="w-full flex items-center justify-center gap-4"
+        >
+          <GiProgression size={24} />
+          Amplie seu Potencial
+        </Button>
+        <Button
+          size={"lg"}
+          variant={"ghost"}
+          className="w-full flex items-center justify-center gap-4"
+        >
+          <FaUserFriends size={24} />
+          Compita com Amigos
+        </Button>
+      </div>
     </footer>
   );
 };
--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,3 +1,72 @@
+import Image from "next/image";
+
+import {
+  ClerkLoaded,
+  ClerkLoading,
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+} from "@clerk/nextjs";
+
+import { Button } from "@/components/ui/button";
+
+import { Loader } from "lucide-react";
+
+import Link from "next/link";
+
 export default function Home() {
-  return <div>LandingPage</div>;
+  return (
+    <div className="max-w-[988px] mx-auto flex-1 w-full flex flex-col lg:flex-row items-center justify-center p-4 gap-2">
+      <div className="relative w-[320px] h-[320px] lg:w-[424px] lg:h-[424px]">
+        <Image src="/hero.png" fill alt="Hero" />
+      </div>
+      <div className="flex flex-col items-center gap-y-8">
+        <h1 className="text-xl lg:text-3xl font-bold text-neutral-600 max-w-[480px] text-center">
+          Pratique e reforce o seu raciocínio lógico com o CortexCraft
+        </h1>
+        <div className="flex flex-col items-center gap-y-3 max-w-[330px] w-full">
+          <ClerkLoading>
+            <Loader className="h-5 w-5 text-muted-foreground animate-spin" />
+          </ClerkLoading>
+          <ClerkLoaded>
+            <SignedOut>
+              <SignUpButton
+                mode="modal"
+                afterSignInUrl="/learn"
+                afterSignUpUrl="/learn"
+              >
+                <Button size={"lg"} variant={"primary"} className="w-full">
+                  Começar
+                </Button>
+              </SignUpButton>
+              <SignInButton
+                mode="modal"
+                afterSignInUrl="/learn"
+                afterSignUpUrl="/learn"
+              >
+                <Button
+                  size={"lg"}
+                  variant={"secondaryOutline"}
+                  className="w-full"
+                >
+                  Eu já tenho uma conta
+                </Button>
+              </SignInButton>
+            </SignedOut>
+            <SignedIn>
+              <Button
+                size={"lg"}
+                variant={"secondary"}
+                className="w-full"
+                asChild
+              >
+                <Link href={"/learn"}>Continue Aprendendo</Link>
+              </Button>
+            </SignedIn>
+          </ClerkLoaded>
+        </div>
+      </div>
+    </div>
+  );
 }
