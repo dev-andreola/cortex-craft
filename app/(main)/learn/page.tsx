fca94dd11429af351ffe21eb467b3a8509ebdf7d
vtAndreola <dev.andreola@gmail.com>
2024-04-16 17:53:03 -0300

    feat: Add user progress

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -2,12 +2,22 @@ import { FeedWrapper } from "@/components/feed-wrapper";
 import { StickyWrapper } from "@/components/sticky-wrapper";
 import { Header } from "./header";
 import { UserProgress } from "@/components/user-progress";
+import { getUserProgress } from "@/db/queries";
+import { redirect } from "next/navigation";
 
-const LearnPage = () => {
+const LearnPage = async () => {
+  const userProgressData = getUserProgress();
+
+  const [userProgress] = await Promise.all([userProgressData]);
+
+  if (!userProgress || !userProgress.activeCourse) {
+    redirect("/courses");
+  }
+
   return (
     <div className="flex gap=[48px] px-6">
       <FeedWrapper>
-        <Header title="Quiz" />
+        <Header title="Quiz 1" />
       </FeedWrapper>
       <StickyWrapper>
         <UserProgress
