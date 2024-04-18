9842a797375c14821ac39e870e4db778e327b097
vtAndreola <dev.andreola@gmail.com>
2024-04-17 21:43:36 -0300

    add: Unit ui

--- actions/user-progress.tsx
+++ actions/user-progress.tsx
@@ -1,29 +1,27 @@
 "use server";
 
+import { and, eq } from "drizzle-orm";
+import { redirect } from "next/navigation";
+import { revalidatePath } from "next/cache";
+import { auth, currentUser } from "@clerk/nextjs";
+
 import db from "@/db/drizzle";
 import { getCourseById, getUserProgress } from "@/db/queries";
 import { userProgress } from "@/db/schema";
-import { auth, currentUser } from "@clerk/nextjs";
-import { revalidatePath } from "next/cache";
-import { redirect } from "next/navigation";
 
 export const upsertUserProgress = async (courseId: number) => {
   const { userId } = await auth();
   const user = await currentUser();
 
   if (!userId || !user) {
-    throw new Error("Não autorizado");
+    throw new Error("Não autorizado!");
   }
 
   const course = await getCourseById(courseId);
 
   if (!course) {
-    throw new Error("Curso não encontrado");
+    throw new Error("Curso não encontrado!");
   }
-
-  //   if (!course.units.length || !course.units[0].lessons.length) {
-  //     throw new Error("Curso está vazio");
-  //   }
 
   const existingUserProgress = await getUserProgress();
 
@@ -49,4 +47,51 @@ export const upsertUserProgress = async (courseId: number) => {
   revalidatePath("/courses");
   revalidatePath("/learn");
   redirect("/learn");
+};
+
+export const reduceHearts = async (challengeId: number) => {
+  const { userId } = await auth();
+
+  if (!userId) {
+    throw new Error("Não autorizado!");
+  }
+
+  const currentUserProgress = await getUserProgress();
+
+  if (!currentUserProgress) {
+    throw new Error("Progresso não encontrado");
+  }
+
+  if (currentUserProgress.hearts === 0) {
+    return { error: "hearts" };
+  }
+
+  await db
+    .update(userProgress)
+    .set({
+      hearts: Math.max(currentUserProgress.hearts - 1, 0),
+    })
+    .where(eq(userProgress.userId, userId));
+
+  revalidatePath("/shop");
+  revalidatePath("/learn");
+  revalidatePath("/quests");
+  revalidatePath("/leaderboard");
+};
+
+export const refillHearts = async () => {
+  const currentUserProgress = await getUserProgress();
+
+  if (!currentUserProgress) {
+    throw new Error("Progresso não encontrado!");
+  }
+
+  if (currentUserProgress.hearts === 5) {
+    throw new Error("Vida já está cheia");
+  }
+
+  revalidatePath("/shop");
+  revalidatePath("/learn");
+  revalidatePath("/quests");
+  revalidatePath("/leaderboard");
 };
