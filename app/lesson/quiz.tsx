d5c4d18b02d9bf93f41bc408bf1f83116b419d2a
vtAndreola <dev.andreola@gmail.com>
2024-04-17 22:41:53 -0300

    feat: Add challenge page

---
+++ app/lesson/quiz.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { challengeOptions, challenges } from "@/db/schema";
+import { useState } from "react";
+import { Header } from "./header";
+
+type Props = {
+  initialPercentage: number;
+  initialHearts: number;
+  initialLessonId: number;
+  initialLessonChallenges: (typeof challenges.$inferSelect & {
+    completed: boolean;
+    challengeOptions: (typeof challengeOptions.$inferSelect)[];
+  })[];
+  userSubscription: any;
+};
+
+export const Quiz = ({
+  initialPercentage,
+  initialHearts,
+  initialLessonId,
+  initialLessonChallenges,
+  userSubscription,
+}: Props) => {
+  const [hearts, setHearts] = useState(initialHearts);
+  const [percentage, setPercentage] = useState(() => {
+    return initialPercentage === 100 ? 0 : initialPercentage;
+  });
+  return (
+    <>
+      <Header
+        hearts={hearts}
+        percentage={60 || percentage}
+        hasActiveSubscription={!!userSubscription?.isActive}
+      />
+    </>
+  );
+};
